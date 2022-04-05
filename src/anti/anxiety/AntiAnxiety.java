c0482765e340bb3e968e48fc4136477ff3f07efb
Saliim11 <qalbunsaliimbakhri@gmail.com>
2022-04-05 22:27:14 +0700

    inisialisasi awal projek

---
+++ src/anti/anxiety/AntiAnxiety.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anti.anxiety;
+
+/**
+ *
+ * @author Qalbun Saliim Bakhri
+ */
+public class AntiAnxiety {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
